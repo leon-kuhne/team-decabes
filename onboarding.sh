f7e8dfa11e8879d0a823b56f6231389484b5cec3
evanr <evan_ristow@ultimatesoftware.com>
2021-10-20 16:47:47 -0400

    add onboarding.sh

--- onboarding.sh
+++ onboarding.sh
@@ -1,1 +1,9 @@
+# gcp cli
+brew install --cask google-cloud-sdk
+
+# qrk cli
+curl "https://artifactory.mia.ulti.io/artifactory/gt-generic-prod/quark-cli/installers/macos/qrk-v3.0.20.pkg" -o qrk-v3.0.20.pkg
+sudo installer -pkg qrk-v3.0.20.pkg -target /
+
 echo "Welcome to Decabes!"
+
